c0de31bdc35af9afe48982e5c530891b1431fa2b
vmontanomena <vicente.montanomena@telefonica.com>
2019-01-26 19:18:55 +0100

    Example: how to add icons

--- App.js
+++ App.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Ionicons } from '@expo/vector-icons'
 
 export default class App extends React.Component {
+
+  componentDidMount() {
+    console.log('Before')
+    // debugger
+    console.log('After')
+  }
+
   render() {
     return (
       <View style={styles.container}>
         <Text>Open up App.js to start working on your app!</Text>
+        <Ionicons name='ios-pizza' color='red' size={100}/>
       </View>
     );
   }
