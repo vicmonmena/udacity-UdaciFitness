703ff3607056fe538b393a1325c6c7474743d60b
vmontanomena <vicente.montanomena@telefonica.com>
2019-01-27 11:45:59 +0100

    AddEntry Component: updating helpers and showing AddEntry content

--- App.js
+++ App.js
@@ -1,30 +1,15 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { Ionicons } from '@expo/vector-icons'
+import React from 'react'
+import { View } from 'react-native'
+import AddEntry from './components/AddEntry'
+
 
 export default class App extends React.Component {
 
-  componentDidMount() {
-    console.log('Before')
-    // debugger
-    console.log('After')
-  }
-
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Ionicons name='ios-pizza' color='red' size={100}/>
+      <View>
+        <AddEntry />
       </View>
-    );
+    )
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ components/AddEntry.js
@@ -0,0 +1,10 @@
+
+import React, { Component } from "react"
+import { View } from "react-native"
+import { getMetricMetaInfo } from "../utils/helpers"
+
+export default class AddEntry extends Component {
+  render() {
+    return <View>{getMetricMetaInfo("bike").getIcon()}</View>
+  }
+}
