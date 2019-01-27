3cd48deee42fbc5949adb1b6d60edc369155f5b8
vmontanomena <vicente.montanomena@telefonica.com>
2019-01-27 21:39:13 +0100

    Adding components for metric recording: Slider & Steppers. Adding Tocuhables

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import React from 'react'
-import { View } from 'react-native'
+import { View, Text } from 'react-native'
 import AddEntry from './components/AddEntry'
 
 
 export default class App extends React.Component {
 
+  
   render() {
     return (
       <View>
