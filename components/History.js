894d0e6a5131d6900e0c70ca761a801dbd410f14
vmontanomena <vicente.montanomena@telefonica.com>
2019-02-01 18:32:11 +0100

    StackNavigator: adding navigation from activity history to detail

--- components/History.js
+++ components/History.js
@@ -37,9 +37,10 @@ class History extends Component {
               {today}
             </Text>
           </View>
-        : <TouchableOpacity
-            onPress={() => console.log('Pressed!')}
-          >
+        : <TouchableOpacity onPress={() => this.props.navigation.navigate(
+              'EntryDetail',
+              { entryId: key }
+            )}>
             <MetricCard date={formattedDate} metrics={metrics} />
           </TouchableOpacity>}
     </View>
